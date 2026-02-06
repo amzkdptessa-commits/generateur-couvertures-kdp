c23fb0cb00364bb91963504dd1fb9eaee3274004
TESSA MARIE <amzkdptessa@gmail.com>
2026-02-06 14:01:42 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/*.html",
-    "./**/*.js",
-  ],
+  // Le pattern "./*.html" dit à Tailwind de regarder TOUS les fichiers .html 
+  // présents dans votre dossier actuel.
+  content: ["./*.html"], 
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
